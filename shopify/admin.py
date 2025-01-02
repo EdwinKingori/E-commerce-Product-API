f1b6c37cfa13177fd405dee08230ca9049a4e3dc
Edwin King'ori <eddmunyiri@gmail.com>
2025-01-02 09:20:30 +0300

    add search fields with lookup types in CustomerAdmin

--- shopify/admin.py
+++ shopify/admin.py
@@ -46,7 +46,9 @@ class ProductAdmin(admin.ModelAdmin):
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'orders_count']
+    list_per_page = 10
     ordering = ['user__first_name', 'user__last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders_count(self, customer):
